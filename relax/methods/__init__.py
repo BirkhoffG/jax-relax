cca6d91a03c9e7229d54e22be189902beb4b4575
Hangzhi Guo <26811230+BirkhoffG@users.noreply.github.com>
2023-05-04 22:42:15 -0400

    Add new methods into `relax.methods.__init__` #145 (#146)

--- relax/methods/__init__.py
+++ relax/methods/__init__.py
@@ -2,3 +2,7 @@ from .vanilla import VanillaCFConfig, VanillaCF
 from .diverse import DiverseCFConfig, DiverseCF
 from .proto import ProtoCFConfig, ProtoCF
 from .counternet import CounterNetConfigs, CounterNet
+from .cchvae import CCHVAEConfigs, CCHVAE
+from .clue import CLUEConfigs, CLUE
+from .sphere import GSConfig, GrowingSphere
+from .vaecf import VAECFConfigs, VAECF
