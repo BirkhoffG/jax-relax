dc75583c35ff7073feb87db23bfad69287dd6415
BirkhoffG <26811230+BirkhoffG@users.noreply.github.com>
2023-06-03 11:33:52 +0000

    Update scalability result

--- scripts/benchmark_scalability.py
+++ scripts/benchmark_scalability.py
@@ -17,7 +17,7 @@ import gc
 CF_NAMES = ["VanillaCF","DiverseCF","ProtoCF","CounterNet","CCHVAE","CLUE","GrowingSphere","VAECF"]
 
 # list of sample fractions
-sample_fracs = [0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+sample_fracs = [0.8, 0.9]
 
 
 class BenchmarkLogger:
