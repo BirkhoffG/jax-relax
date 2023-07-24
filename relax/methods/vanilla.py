f088c196b91594405b0accb0396cd97eee4d3124
BirkhoffG <26811230+BirkhoffG@users.noreply.github.com>
2023-07-24 11:03:10 -0400

    Implement VanillaCF

--- relax/methods/vanilla.py
+++ relax/methods/vanilla.py
@@ -3,29 +3,33 @@
 # %% ../../nbs/methods/01_vanilla.ipynb 3
 from __future__ import annotations
 from ..import_essentials import *
-from .base import BaseCFModule
-from ..utils import *
+from .base import CFModule
+from ..base import BaseConfig
+from ..utils import auto_reshaping, grad_update, validate_configs
 
 # %% auto 0
 __all__ = ['VanillaCFConfig', 'VanillaCF']
 
-# %% ../../nbs/methods/01_vanilla.ipynb 4
+# %% ../../nbs/methods/01_vanilla.ipynb 5
 @auto_reshaping('x')
 def _vanilla_cf(
     x: jnp.DeviceArray,  # `x` shape: (k,), where `k` is the number of features
-    pred_fn: Callable[[jnp.DeviceArray], jnp.DeviceArray],  # y = pred_fn(x)
+    y_target: Array, # `y_target` shape: (1,)
+    pred_fn: Callable[[Array], Array],  # y = pred_fn(x)
     n_steps: int,
     lr: float,  # learning rate for each `cf` optimization step
     lambda_: float,  #  loss = validity_loss + lambda_params * cost
-    apply_fn: Callable
+    validity_fn: Callable,
+    cost_fn: Callable,
+    apply_constraints_fn: Callable
 ) -> jnp.DeviceArray:  # return `cf` shape: (k,)
     @jit
-    def loss_fn_1(cf_y: Array, y_prime: Array):
-        return jnp.mean(binary_cross_entropy(preds=cf_y, labels=y_prime))
+    def loss_fn_1(y_true: Array, y_pred: Array):
+        return validity_fn(y_true, y_pred).mean()
 
     @jit
     def loss_fn_2(x: Array, cf: Array):
-        return jnp.mean(optax.l2_loss(cf, x))
+        return cost_fn(cf, x).mean()
 
     @partial(jit, static_argnums=(2,))
     def loss_fn(
@@ -34,9 +38,9 @@ def _vanilla_cf(
         pred_fn: Callable[[Array], Array],
     ):
         y_pred = pred_fn(x)
-        y_prime = 1.0 - y_pred
-        cf_y = pred_fn(cf)
-        return loss_fn_1(cf_y, y_prime) + lambda_ * loss_fn_2(x, cf)
+        cf_y_true = 1.0 - y_pred
+        cf_y_pred = pred_fn(cf)
+        return loss_fn_1(cf_y_true, cf_y_pred) + lambda_ * loss_fn_2(x, cf)
 
     @loop_tqdm(n_steps)
     def gen_cf_step(
@@ -45,7 +49,7 @@ def _vanilla_cf(
         cf, opt_state = cf_opt_state
         cf_grads = jax.grad(loss_fn)(cf, x, pred_fn)
         cf, opt_state = grad_update(cf_grads, cf, opt_state, opt)
-        cf = apply_fn(x, cf, hard=False)
+        cf = apply_constraints_fn(x, cf, hard=False)
         return cf, opt_state
 
     cf = jnp.array(x, copy=True)
@@ -55,53 +59,52 @@ def _vanilla_cf(
     #     cf, opt_state = gen_cf_step(x, cf, opt_state)
     cf, opt_state = lax.fori_loop(0, n_steps, gen_cf_step, (cf, opt_state))
 
-    cf = apply_fn(x, cf, hard=True)
+    cf = apply_constraints_fn(x, cf, hard=True)
     return cf
 
-
-# %% ../../nbs/methods/01_vanilla.ipynb 5
-class VanillaCFConfig(BaseParser):
-    n_steps: int = 1000
-    lr: float = 0.001
-    lambda_: float = 0.01  # loss = validity_loss + lambda_ * cost
-
-
 # %% ../../nbs/methods/01_vanilla.ipynb 6
-class VanillaCF(BaseCFModule):
-    name = "VanillaCF"
+class VanillaCFConfig(BaseConfig):
+    n_steps: int = 100
+    lr: float = 0.1
+    lambda_: float = 0.1
+    validity_fn: str = 'KLDivergence'
+
+# %% ../../nbs/methods/01_vanilla.ipynb 7
+class VanillaCF(CFModule):
 
     def __init__(
         self,
-        configs: dict | VanillaCFConfig = None
+        configs: dict | VanillaCFConfig = None,
+        name: str = None,
     ):
         if configs is None:
             configs = VanillaCFConfig()
-        self.configs = validate_configs(configs, VanillaCFConfig)
+        configs = validate_configs(configs, VanillaCFConfig)
+        name = "VanillaCF" if name is None else name
+        super().__init__(configs, name=name)
 
+    @auto_reshaping('x')
     def generate_cf(
         self,
-        x: jnp.ndarray,  # `x` shape: (k,), where `k` is the number of features
-        pred_fn: Callable[[jnp.DeviceArray], jnp.DeviceArray],
+        x: Array,  # `x` shape: (k,), where `k` is the number of features
+        pred_fn: Callable[[Array], Array],
+        y_target: Array = None,
     ) -> jnp.DeviceArray:
+        # TODO: Currently assumes binary classification.
+        if y_target is None:
+            y_target = 1 - pred_fn(x)
+        else:
+            y_target = jnp.array(y_target, copy=True)
+
         return _vanilla_cf(
             x=x,  # `x` shape: (k,), where `k` is the number of features
+            y_target=y_target,  # `y_target` shape: (1,)
             pred_fn=pred_fn,  # y = pred_fn(x)
-            n_steps=self.configs.n_steps,
-            lr=self.configs.lr,  # learning rate for each `cf` optimization step
-            lambda_=self.configs.lambda_,  #  loss = validity_loss + lambda_params * cost
-            apply_fn=self.data_module.apply_constraints
-        )
-
-    def generate_cfs(
-        self,
-        X: jnp.DeviceArray,  # `x` shape: (b, k), where `b` is batch size, `k` is the number of features
-        pred_fn: Callable[[jnp.DeviceArray], jnp.DeviceArray],
-        is_parallel: bool = False,
-    ) -> jnp.DeviceArray:
-        def _generate_cf(x: jnp.DeviceArray) -> jnp.ndarray:
-            return self.generate_cf(x, pred_fn)
-
-        return (
-            jax.vmap(_generate_cf)(X) if not is_parallel else jax.pmap(_generate_cf)(X)
+            n_steps=self.config.n_steps,
+            lr=self.config.lr,  # learning rate for each `cf` optimization step
+            lambda_=self.config.lambda_,  #  loss = validity_loss + lambda_params * cost
+            validity_fn=keras.losses.get({'class_name': self.config.validity_fn, 'config': {'reduction': None}}),
+            cost_fn=keras.losses.get({'class_name': 'MeanSquaredError', 'config': {'reduction': None}}),
+            apply_constraints_fn=self.apply_constraints_fn,
         )
 
